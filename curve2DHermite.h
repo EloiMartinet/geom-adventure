0413b03541374026609a571cdf802feb7102457c
Oussama Tchita <tchitao@f203-01.e-im2ag.ujf-grenoble.fr>
2017-11-29 18:22:37 +0100

    hermite done

--- curve2DHermite.h
+++ curve2DHermite.h
@@ -4,6 +4,7 @@
 #include "curve2D.h"
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -12,15 +13,15 @@ class Curve2DHermite : public Curve2D {
  Curve2DHermite(const QString &name) : Curve2D(name) {}
  Curve2DHermite(Curve2D *curve,const QString &name) : Curve2D(curve,name) {}
 
-vector< vector <double> > hermiteInter(vector<double> const checkPoint_1, vector<double> const checkPoint_2, double h)
+vector< vector <double> > hermiteInter(vector<double> const checkPoint_0, vector<double> const checkPoint_1, vector<double> const checkPoint_2, unsigned int N)
 {
-    unsigned int N = (int)(1/h);
     vector< vector<double> > H(N);
     //vector< vector<double> > li(N, (double ) 1);
     //vector<double> dy(1,N);
     //vector<double> x(N);
     //vector<double> y(N);
-    double a = (checkPoint_2[1] - checkPoint_1[1])/(checkPoint_2[0] - checkPoint_1[0]);
+    double a1 = (checkPoint_1[1] - checkPoint_0[1])/(checkPoint_1[0] - checkPoint_0[0]);
+    double a2 = (checkPoint_2[1] - checkPoint_1[1])/(checkPoint_2[0] - checkPoint_1[0]);
     //double b = checkPoint_1[1] - a * checkPoint_1[0];
     /*
     x[0] = checkPoint_1[0];
@@ -38,13 +39,15 @@ vector< vector <double> > hermiteInter(vector<double> const checkPoint_1, vector
     */
     for (unsigned int t = 0; t < N; t++) {
       vector<double> v(2);
-      double s = (double) t/N;
+      double s = (double) t*(checkPoint_2[0] - checkPoint_1[0])/(N-1);
+      double c = (checkPoint_2[0] - checkPoint_1[0]);
+      s /= c;
       double h1 = 2*pow(s, 3) - 3*pow(s, 2) + 1;
       double h2 = (-2)*pow(s, 3) + 3*pow(s, 2);
       double h3 = pow(s, 3) - 2*pow(s, 2) + s;
       double h4 = pow(s, 3) - pow(s, 2);
       for (size_t i = 0; i < 2; i++) {
-        v[i] = h1*checkPoint_1[i] + h2*checkPoint_2[i] + h3*a + h4*a;
+        v[i] = h1*checkPoint_1[i] + h2*checkPoint_2[i] + h3*a1*c + h4*a2*c;
       }
       H[t] = v;
     }
@@ -88,10 +91,15 @@ vector< vector <double> > hermiteInter(vector<double> const checkPoint_1, vector
       checkPoint[i].push_back(pt[0]);
       checkPoint[i].push_back(pt[1]);
     }
-    for(unsigned int i=1;i<nbPts()-1;++i) {
-      double h;
-      h = 1/N;
-      H = hermiteInter(checkPoint[i], checkPoint[i+1], h);
+    for(unsigned int i=0;i<nbPts()-1;++i) {
+      if (i == 0) {
+        vector<double> tmp(2);
+        tmp[0] = 2*checkPoint[0][0] - checkPoint[1][0];
+        tmp[1] = 2*checkPoint[0][1] - checkPoint[1][1];
+        H = hermiteInter(tmp, checkPoint[i], checkPoint[i+1], N);
+      } else {
+        H = hermiteInter(checkPoint[i-1], checkPoint[i], checkPoint[i+1], N);
+      }
       for (int k(0); k<N; k++){
           p.lineTo(H[k][0],H[k][1]);
       }
